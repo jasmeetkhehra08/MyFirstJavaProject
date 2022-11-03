4219ce58be4e29adf25799e2e1c07477df8d15b0
jasmeetkhehra08 <jasskhehra26@gmail.com>
2022-11-03 01:56:21 -0400

    assignment week 9

--- src/week9/GameRules.java
+++ src/week9/GameRules.java
@@ -6,22 +6,29 @@ public class GameRules {
 
 	void guessedLetter(String name, String nameMasked) {
 		Scanner sc1= new Scanner(System.in);
+		int counter=0;
+		nameMasked="";
+		do {
 		System.out.println("You are guessing:" +nameMasked);
+		System.out.println("You have guessed ("+counter+ ") wrong letters");
 		System.out.println("Guess a letter:");
 		String letter = sc1.next();
 		char[] nameToCharArray = name.toCharArray();
-		nameMasked="";
+		
 		for (int i = 0; i < nameToCharArray.length; i++) {
 			if (nameToCharArray[i] == letter.charAt(0)) {
+				
 				nameMasked=nameMasked +letter;
 				
-				
-			} else {
-				nameMasked += "_";
-				
+			}	
+			else {
+				nameMasked=nameMasked + "_";
 			}	
 }
 		System.out.println(nameMasked);
+		if(nameMasked=="_") {counter++;
+		}
 	
+}while(counter<6);
 }
 }
